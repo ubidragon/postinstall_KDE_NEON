69c5a1624c876e4cf3f08538d2396fcaf2e83267
ubidragon <ubidragon@protonmail.com>
2019-09-22 13:36:26 +0200

    fix hardboiled

--- installation/add_ppa.sh
+++ installation/add_ppa.sh
@@ -21,11 +21,17 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 614C4B38
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1378B444
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 886DDD89
+#Typora
 curl -fsSL https://typora.io/linux/public-key.asc | sudo apt-key add -
+#Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+#Vivaldi
 curl -fsSL http://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+#Balena Etcher
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+#Insync
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ACCAF35C
 #sudo apt-key add oracle_vbox_2016.asc
 #sudo apt-key add oracle_vbox.asc
 
@@ -47,8 +53,7 @@ sudo add-apt-repository -y ppa:nilarimogard/webupd8
 #F.lux
 sudo add-apt-repository -y ppa:nathan-renniewaldock/flux
 #Sublime Text 3
-sudo echo "deb http://ppa.launchpad.net/webupd8team/sublime-text-3/ubuntu artful main " >> /etc/apt/sources.list
-sudo echo "deb-src http://ppa.launchpad.net/webupd8team/sublime-text-3/ubuntu artful main " >> /etc/apt/sources.list
+echo "deb http://ppa.launchpad.net/webupd8team/sublime-text-3/ubuntu artful main "| sudo tee -a /etc/apt/sources.list.d/sublimetext3.list
 #Brackets
 sudo add-apt-repository -y ppa:webupd8team/brackets
 #Typora
@@ -60,9 +65,9 @@ sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/u
 #Kubernetes (kubectl)
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 #SoftEther (vpn)
-sudo add-apt-repository ppa:paskal-07/softethervpn
+sudo add-apt-repository -y ppa:paskal-07/softethervpn
 #Virtualbox
-echo "deb https://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
+echo "deb https://download.virtualbox.org/virtualbox/ubuntu $(lsb_release -sc) contrib" | sudo tee -a /etc/apt/sources.list.d/virtualbox.list
 #Remember The Milk
 echo "deb https://www.rememeberthemilk.com/download/linux/debian jessie main" | sudo tee -a /etc/apt/sources.list.d/rtm.list
 #Insync
--- installation/install_hardboiled.sh
+++ installation/install_hardboiled.sh
@@ -19,20 +19,20 @@ cd ~/Descargas/
 
 #-----install gitkraken---------------------------------------------------------------------------------
 sudo mkdir gitkraken && cd gitkraken
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 cd ~/Descargas/
-rm -rf gitkraken
+sudo rm -rf gitkraken
 
 #-----install flameshot---------------------------------------------------------------------------------
 sudo mkdir flameshot && cd flameshot
-wget https://github.com/lupoDharkael/flameshot/releases/download/v0.6.0/flameshot_0.6.0_bionic_x86_64.deb
+sudo wget https://github.com/lupoDharkael/flameshot/releases/download/v0.6.0/flameshot_0.6.0_bionic_x86_64.deb
 sudo dpkg -i flameshot_*.deb
 cd ~/Descargas/
-rm -rf anydesk
+sudo rm -rf flameshot
 
 #-----config docker--------------------------------------------------------------------------------
-sudo usermod -aG docker $USER
+sudo usesudo rmod -aG docker $USER
 
 #-----GUI Portainer---------------------------------------------------------------------------------
 docker volume create portainer_data
@@ -45,25 +45,25 @@ sudo install minikube /usr/local/bin
 
 #-----install Anydesk--------------------------------------------------------------------------------
 sudo mkdir anydesk && cd anydesk
-wget https://download.anydesk.com/linux/anydesk_5.1.2-1_amd64.deb
+sudo wget https://download.anydesk.com/linux/anydesk_5.1.2-1_amd64.deb
 sudo dpkg -i anydesk*.deb
 cd ~/Descargas/
-rm -rf anydesk
+sudo rm -rf anydesk
 
 #-----install Autofirma--------------------------------------------------------------------------------
 sudo mkdir autofirma && cd autofirma
-wget https://estaticos.redsara.es/comunes/autofirma/currentversion/AutoFirma_Linux.zip
+sudo wget https://estaticos.redsara.es/comunes/autofirma/currentversion/AutoFirma_Linux.zip
 unzip AutoFirma_Linux.zip -d autofirma/
 sudo dpkg -i AutoFirma*.deb
 cd ~/Descargas/
-rm -rf autofirma
+sudo rm -rf autofirma
 
 #-----install Openfortigui--------------------------------------------------------------------------------
 sudo mkdir openforti && cd openforti
-wget https://apt.iteas.at/iteas/pool/main/o/openfortigui/openfortigui_0.8.0-1_amd64_bionic.deb
+sudo wget https://apt.iteas.at/iteas/pool/main/o/openfortigui/openfortigui_0.8.0-1_amd64_bionic.deb
 sudo dpkg -i openfortigui*.deb
 cd ~/Descargas/
-rm -rf openforti
+sudo rm -rf openforti
 
 #-----install oh_my_zsh--------------------------------------------------------------------------------
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
@@ -75,7 +75,7 @@ sudo make install
 
 #-----install ocs-url----------------------------------------------------------------------------------
 mkdir ocs && cd ocs
-wget https://dl.opendesktop.org/api/files/download/id/1530774600/s/9176c91728d072580279f000eb7723cc7a9ea065d9ca070b20e357f002bc9c40c8771ae8ab7072c5fc18cabfa931356676673eaceb4c0e185a6e886a18999718/t/1565462431/lt/download/ocs-url_3.1.0-0ubuntu1_amd64.deb
+sudo wget https://dl.opendesktop.org/api/files/download/id/1530774600/s/9176c91728d072580279f000eb7723cc7a9ea065d9ca070b20e357f002bc9c40c8771ae8ab7072c5fc18cabfa931356676673eaceb4c0e185a6e886a18999718/t/1565462431/lt/download/ocs-url_3.1.0-0ubuntu1_amd64.deb
 sudo dpkg -i ocs-*.deb
 cd ~/Descargas/
-rm -rf ocs
+sudo rm -rf ocs
